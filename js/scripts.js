cc2e965fe6ddafdf511f91863e4a6967149bb1d8
sowmyadsl <sowmya.dsl@gmail.com>
2017-03-23 19:20:07 -0700

    Added jquery to change the header of the page after user submits the order

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,20 @@
 $(function() {
-  $("#purchase").submit(function(event) {
-    var productInput = $("input#product").val();
+  $("#formOrder").submit(function(event) {
+    var firstnameInput = $("input#first-name").val();
+    var lastnameInput = $("input#last-name").val();
+    var categoryInput = $("input:radio[name=category]:checked").val();
+    var productnameInput = $("input#product-name").val();
     var quantityInput = $("input#quantity").val();
-    var notesInput = $("input#notes").val();
-
-    $("#text1").text(productInput);
-    $("#text2").text(quantityInput);
-    $("#text3").text(notesInput);
-    $("#purchase-form").hide();
-    $("#shipping-form").show();
-    event.preventDefault();
-  });
-  $("#shipping").submit(function(event) {
-    var nameInput = $("input#name").val();
-    var addressInput = $("input#address").val();
-    var shippingInput = $("input:radio[name=shipping]:checked").val();
-
-    $("#text4").text(nameInput);
-    $("#text5").text(addressInput);
+    var shippingInput = $("input#shipping-address").val();
+    $("#text1").text(firstnameInput);
+    $("#text2").text(lastnameInput);
+    $("#text3").text(categoryInput);
+    $("#text4").text(productnameInput);
+    $("#text5").text(quantityInput);
     $("#text6").text(shippingInput);
-
-    $("#shipping-form").hide();
+    $("#formOrder").hide();
     $("#receipt").show();
+    $("h2").replaceWith("<h2>Order Receipt Confirmation</h2>");
     event.preventDefault();
   });
 });
